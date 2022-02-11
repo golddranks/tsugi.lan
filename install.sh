fd046d11ccdfa744c6242a5965f4d35ecb222b5b
Pyry Kontio <pyry.kontio@drasa.eu>
2022-02-12 05:01:49 +0900

    Rewrote for VLANing for passing through WAN to Mon. Added 802.11r (WiFi roaming). Changed router SSH port to 222. Fixed WPS.

--- install.sh
+++ install.sh
@@ -1,33 +1,38 @@
 #!/bin/sh -eu
 
-echo "Did you run ./connect.sh, i.e. is the router connected to the LAN and internet and accessible from the current host?"
-
-CURRENT_IP=${1:-10.0.0.2}
+CURRENT_IP=${1:-192.168.1.1}
 
 [ ! -d "../certs" ] && echo "Prepare ../certs directory" && exit 1
 [ ! -d "../pubkeys" ] && echo "Prepare ../pubkeys directory" && exit 1
 
 . ./secrets.sh
-
 SSH_PUBKEY=$(cat $HOME/.ssh/id_rsa.pub)
 
-# Dropping the host key using SCP because the dropbear format is a binary format
+# Sending the host key with SCP because the dropbear format is a binary format
 echo "$DROPBEAR_HOST_KEY" | base64 --decode > dropbear_ed25519_host_key
 scp -o StrictHostKeyChecking=no dropbear_ed25519_host_key \
     "root@${CURRENT_IP}:/etc/dropbear/dropbear_ed25519_host_key"
 rm dropbear_ed25519_host_key
-ssh -o StrictHostKeyChecking=no root@${CURRENT_IP} "/etc/init.d/dropbear restart"
+ssh -o StrictHostKeyChecking=no "root@${CURRENT_IP}" "/etc/init.d/dropbear restart"
 
 echo "Dropbear restarted."
 ssh-keygen -R "${CURRENT_IP}"
 
-# x509 certs for the admin panel are also unwieldy to handle so let's SCP them
-ssh -o StrictHostKeyChecking=no "root@${CURRENT_IP}" "mkdir -p /etc/ssl"
+# Logging in once to re-set the correct key on this host
+ssh -o StrictHostKeyChecking=no "root@${CURRENT_IP}" "echo Works!"
+
+# x509 certs for the admin panel are also unwieldy so let's SCP them
+ssh "root@${CURRENT_IP}" "mkdir -p /etc/ssl"
 scp ../certs/cert/tsugi.lan.key "root@${CURRENT_IP}:/etc/ssl/tsugi.lan.key"
 scp ../certs/cert/tsugi.lan.chain.pem "root@${CURRENT_IP}:/etc/ssl/tsugi.lan.chain.pem"
 
-# Sending the install scripts
+# Sending the install script
 scp remote.sh "root@${CURRENT_IP}:"
+
+# Sending the authorized pub keys
 scp ../pubkeys/authorized_keys_strict "root@${CURRENT_IP}:"
+
+# Running the install script
 ssh "root@${CURRENT_IP}" "./remote.sh '$ROOT_PW' '$SSH_PUBKEY' '$WIFI_PW'"
+echo "Booting."
 ssh "root@${CURRENT_IP}" "reboot now"
--- remote.sh
+++ remote.sh
@@ -27,54 +27,12 @@ uci commit dropbear
 
 echo "Security config done."
 
-
-# Lan IPv6
-
-uci set network.lan6=interface
-uci set network.lan6.proto='dhcpv6'
-uci set network.lan6.device='br-lan'
-uci set network.lan6.ifaceid='::2'
-
-# MacOS NDP+RA IPv6 address selection supports only LLA source addresses, so don't use ULA:
-uci set network.globals.ula_prefix=''
-uci commit network
-
-
-# Add lan6 to trusted zone
-uci delete firewall.cfg02dc81.network
-uci add_list firewall.cfg02dc81.network='lan'
-uci add_list firewall.cfg02dc81.network='lan6'
-uci commit firewall
-
-# Set LAN to relay mode to support NDP+RA based IPv6 addressing
-uci set dhcp.lan.ignore='1'
-uci commit dhcp
-
-# Wifi
-# Note the 2.5Ghz and 5Ghz radio numbers are the other way around as mon!
-uci set wireless.default_radio0.ssid='Skeletor 2.5Ghz'
-uci set wireless.default_radio0.key="$WIFI_PW"
-uci set wireless.default_radio0.encryption='psk2'
-uci set wireless.radio0.disabled='0'
-uci set wireless.radio0.country='JP'
-uci set wireless.default_radio1.ssid='Skeletor 5Ghz'
-uci set wireless.default_radio1.key="$WIFI_PW"
-uci set wireless.default_radio1.encryption='psk2'
-uci set wireless.radio1.disabled='0'
-uci set wireless.radio1.country='JP'
-uci commit wireless
-
 # General system settings
 uci set system.cfg01e48a.hostname='tsugi'
 uci set system.cfg01e48a.timezone='JST-9'
 uci set system.cfg01e48a.zonename='Asia/Tokyo'
 uci commit system
 
-/etc/init.d/odhcpd disable
-/etc/init.d/firewall disable
-/etc/init.d/dnsmasq disable
-
-echo "Basic network config done."
 
 echo "Start installing external packages."
 
@@ -89,10 +47,15 @@ echo "Utilities installed."
 # Install nginx to support performant HTTPS admin panel
 opkg install luci-ssl-nginx
 
+uci delete nginx._lan.listen || true
+uci delete nginx._lan.uci_manage_ssl || true
+uci add_list nginx._lan.listen='666 ssl default_server'
+uci add_list nginx._lan.listen='[::]:666 ssl default_server'
 uci set nginx._lan.ssl_certificate='/etc/ssl/tsugi.lan.chain.pem'
 uci set nginx._lan.ssl_certificate_key='/etc/ssl/tsugi.lan.key'
 
 uci commit nginx
+
 echo "HTTPS enabled on web interface."
 
 # Set up WPS
@@ -103,13 +66,94 @@ opkg install wpad-wolfssl hostapd-utils
 uci set wireless.default_radio0.wps_pushbutton='1'
 uci commit wireless
 
+# NOTE: wlan0 is 2.5Ghz in case of Tsugi!
 cat << EOF > /root/wps.sh
 #!/bin/sh
-hostapd_cli -i wlan1 wps_pbc
-hostapd_cli -i wlan1 wps_get_status
+hostapd_cli -i wlan0 wps_pbc
+hostapd_cli -i wlan0 wps_get_status
 EOF
 chmod 0755 /root/wps.sh
 
 echo "WPS settings done."
+
+
+# Network config last, because it will disable network connectivity from WAN.
+
+# This router is LAN only, so disabling DHCP, DNS, Firewall and deleting WAN interfaces
+/etc/init.d/odhcpd disable
+/etc/init.d/firewall disable
+/etc/init.d/dnsmasq disable
+uci delete network.wan || true
+uci delete network.wan6 || true
+
+# Vlan
+# Delete br-lan, we are going to bridge the wan port too.
+uci delete network.cfg030f15 || true
+uci set network.br0=device
+uci set network.br0.type='bridge'
+uci set network.br0.name="br0"
+uci set network.br0.vlan_filtering='1'
+uci set network.br0.ports='lan1 lan2 lan3 lan4 wan'
+
+# LAN VLAN 1 (LAN1 + LAN2 + LAN3 + LAN4 + ETH0)
+uci set network.br_lan=bridge-vlan
+uci set network.br_lan.device='br0'
+uci set network.br_lan.vlan='1'
+uci set network.br_lan.ports='lan1:t lan2 lan3 lan4'
+uci set network.br_lan.local='1'
+
+# JCOM WAN VLAN 3 (LAN1 + WAN)
+uci set network.br_wan=bridge-vlan
+uci set network.br_wan.device='br0'
+uci set network.br_wan.vlan='3'
+uci set network.br_wan.ports='lan1:t wan'
+uci set network.br_wan.local='0'
+
+# Lan
+uci set network.lan.proto='dhcp'
+uci set network.lan.device='br0.1'
+
+uci set network.lan6=interface
+uci set network.lan6.proto='dhcpv6'
+uci set network.lan6.ifaceid='::2'
+uci set network.lan6.device='br0.1'
+
+uci set firewall.cfg02dc81.network='lan lan6'
+uci commit firewall
+
+# MacOS NDP+RA IPv6 address selection supports only LLA source addresses, so don't use ULA:
+uci set network.globals.ula_prefix=''
+uci commit network
+
+# Wifi
+# Note the 2.5Ghz and 5Ghz radio numbers are the other way around as mon!
+uci set wireless.default_radio0.ssid='Skeletor 2.5Ghz'
+uci set wireless.default_radio0.key="$WIFI_PW"
+uci set wireless.default_radio0.encryption='psk2'
+uci set wireless.radio0.disabled='0'
+uci set wireless.radio0.country='JP'
+uci set wireless.default_radio0.ieee80211r='1'
+uci set wireless.default_radio0.mobility_domain='cc66'
+uci set wireless.default_radio0.ft_over_ds='1'
+uci set wireless.default_radio0.ft_psk_generate_local='1'
+uci set wireless.radio0.cell_density='0'
+
+uci set wireless.default_radio1.ssid='Skeletor 5Ghz'
+uci set wireless.default_radio1.key="$WIFI_PW"
+uci set wireless.default_radio1.encryption='psk2'
+uci set wireless.radio1.disabled='0'
+uci set wireless.radio1.country='JP'
+uci set wireless.default_radio1.ieee80211r='1'
+uci set wireless.default_radio1.mobility_domain='cc66'
+uci set wireless.default_radio1.ft_over_ds='1'
+uci set wireless.default_radio1.ft_psk_generate_local='1'
+uci set wireless.radio1.cell_density='0'
+
+uci commit wireless
+
+echo "Basic network config done."
+
+uci set dropbear.cfg014dd4.Port='222'
+uci commit dropbear
 
 reload_config
